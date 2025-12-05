dd4840a56ae373085e0338524153677eff03bcef
Edriane0226 <edriane.bangonon26@gmail.com>
2025-12-05 21:58:10 +0800

    added shefl seeder sample

---
+++ app/Database/Seeds/ShelfSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Database\Seeds;
+
+use CodeIgniter\Database\Seeder;
+
+class ShelfSeeder extends Seeder
+{
+    public function run()
+    {
+        $data = [
+            [
+                'shelf_id'    => 'SHELF001',
+                'loc_descrip' => 'First Floor - Aisle 1',
+            ],
+            [
+                'shelf_id'    => 'SHELF002',
+                'loc_descrip' => 'First Floor - Aisle 2',
+            ],
+            [
+                'shelf_id'    => 'SHELF003',
+                'loc_descrip' => 'Second Floor - Aisle 1',
+            ],
+        ];
+    }
+}
